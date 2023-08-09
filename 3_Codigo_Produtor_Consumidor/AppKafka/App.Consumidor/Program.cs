59b1cda605886291587f3e6e900a0e6070cff8fa
jeffconexion <jeff.debian2016@gmail.com>
2023-08-09 06:30:54 -0300

    Treinamento kafka.

---
+++ 3_Codigo_Produtor_Consumidor/AppKafka/App.Consumidor/Program.cs
@@ -0,0 +1,16 @@
+﻿using Confluent.Kafka;
+
+var config = new ConsumerConfig
+{
+    GroupId = "devTreinamento",  // Definimos o ID do grupo para o consumidor
+    BootstrapServers = "localhost:9092"  // Definimos o endereço do servidor Kafka
+};
+
+using var consumer = new ConsumerBuilder<string, string>(config).Build();  // Cria um consumidor Kafka com base na configuração fornecida
+consumer.Subscribe("topico-teste");  // Inscreve o consumidor no tópico especificado
+
+while (true)  // Loop infinito para receber continuamente mensagens
+{
+    var result = consumer.Consume();  // Aguarda e consome uma mensagem do tópico inscrito
+    Console.WriteLine($"Messagem: {result.Message.Key}-{result.Message.Value}");  // Exibe a chave e o valor da mensagem consumida no console
+}
---
+++ 3_Codigo_Produtor_Consumidor/AppKafka/src/App.Produtor/Program.cs
@@ -0,0 +1,21 @@
+﻿using Confluent.Kafka;  // Importação do namespace Confluent.Kafka
+
+var config = new ProducerConfig { BootstrapServers = "localhost:9092" };  // Criação da configuração do produtor Kafka com o endereço do servidor
+using var producer = new ProducerBuilder<string, string>(config).Build();  // Criação do produtor Kafka
+
+var message = new Message<string, string>  // Criação da mensagem a ser produzida
+{
+    Key = Guid.NewGuid().ToString(),  // Geração de uma chave aleatória usando Guid
+    Value = $"Mensagem teste - {Guid.NewGuid()}"  // Geração de um valor aleatório para a mensagem
+};
+
+try  // Bloco de código a ser executado e que pode gerar exceções
+{
+    var result = await producer.ProduceAsync("topico-teste", message);  // Produção assíncrona da mensagem no tópico especificado
+    Console.WriteLine($"{result.Offset}");  // Impressão do deslocamento (offset) da mensagem
+    Console.WriteLine("Programa executado com sucesso!");  // Impressão de uma mensagem de sucesso
+}
+catch (Exception ex)  // Captura e tratamento de exceções
+{
+    Console.WriteLine($"Erro ao produzir mensagem: {ex.Message}");  // Impressão de uma mensagem de erro com a descrição da exceção
+}
