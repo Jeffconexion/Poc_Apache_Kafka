070a72038eb4dfabde0d8c70915895666ddf98c0
jeffconexion <jeff.debian2016@gmail.com>
2023-08-11 07:00:11 -0300

    implementação do schema

---
+++ 3_Codigo_Produtor_Consumidor/AppKafka/src/App.Consumidor/Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+//configuração do arquivo yml
+using Confluent.Kafka;
+using Confluent.Kafka.SyncOverAsync;
+using Confluent.SchemaRegistry;
+using Confluent.SchemaRegistry.Serdes;
+
+var schemaConfig = new SchemaRegistryConfig
+{
+    Url = "http://localhost:8081"
+};
+
+var schemaRegistry = new CachedSchemaRegistryClient(schemaConfig);
+
+
+var config = new ConsumerConfig
+{
+    GroupId = "devTreinamento",  // Definimos o ID do grupo para o consumidor
+    BootstrapServers = "localhost:9092"  // Definimos o endereço do servidor Kafka
+};
+
+// Cria um consumidor Kafka com base na configuração fornecida
+//using var consumer = new ConsumerBuilder<string, string>(config).Build();  
+
+using var consumer = new ConsumerBuilder<string, treinamento.Curso>(config)
+                                                                       .SetValueDeserializer(new AvroDeserializer<treinamento.Curso>(schemaRegistry).AsSyncOverAsync())
+                                                                       .Build();  //~~> Criação do produtor Kafka com ~~> schema
+
+
+consumer.Subscribe("Cursos");  // Inscreve o consumidor no tópico especificado
+
+while (true)  // Loop infinito para receber continuamente mensagens
+{
+    var result = consumer.Consume();  // Aguarda e consome uma mensagem do tópico inscrito
+    //Console.WriteLine($"Messagem: {result.Message.Key}-{result.Message.Value}");  // Exibe a chave e o valor da mensagem consumida no console
+    Console.WriteLine($"Messagem: {result.Message.Value.Descricao}");  // Exibe a descrição do Schema
+}
--- 3_Codigo_Produtor_Consumidor/AppKafka/src/App.Produtor/Program.cs
+++ 3_Codigo_Produtor_Consumidor/AppKafka/src/App.Produtor/Program.cs
@@ -1,17 +1,44 @@
 ﻿using Confluent.Kafka;  // Importação do namespace Confluent.Kafka
+using Confluent.SchemaRegistry;
+using Confluent.SchemaRegistry.Serdes;
+
+//configuração do arquivo yml
+var schemaConfig = new SchemaRegistryConfig
+{
+    Url = "http://localhost:8081"
+};
+
+var schemaRegistry = new CachedSchemaRegistryClient(schemaConfig);
+
 
 var config = new ProducerConfig { BootstrapServers = "localhost:9092" };  // Criação da configuração do produtor Kafka com o endereço do servidor
-using var producer = new ProducerBuilder<string, string>(config).Build();  // Criação do produtor Kafka
+//using var producer = new ProducerBuilder<string, string>(config).Build();  //~~> Criação do produtor Kafka
 
-var message = new Message<string, string>  // Criação da mensagem a ser produzida
+using var producer = new ProducerBuilder<string, treinamento.Curso>(config)
+                     .SetValueSerializer(new AvroSerializer<treinamento.Curso>(schemaRegistry))
+                     .Build();  //~~> Criação do produtor Kafka com ~~> schema
+
+// Criação da mensagem a ser produzida
+//var message = new Message<string, string>  
+//{
+//    Key = Guid.NewGuid().ToString(),  // Geração de uma chave aleatória usando Guid
+//    Value = $"Mensagem teste - {Guid.NewGuid()}"  // Geração de um valor aleatório para a mensagem
+//};
+
+// Criação da mensagem a ser produzida com Schema
+var message = new Message<string, treinamento.Curso>
 {
-    Key = Guid.NewGuid().ToString(),  // Geração de uma chave aleatória usando Guid
-    Value = $"Mensagem teste - {Guid.NewGuid()}"  // Geração de um valor aleatório para a mensagem
+    Key = Guid.NewGuid().ToString(),
+    Value = new treinamento.Curso
+    {
+        Id = Guid.NewGuid().ToString(),
+        Descricao = "Curso de Apache Kafka"
+    }
 };
 
 try  // Bloco de código a ser executado e que pode gerar exceções
 {
-    var result = await producer.ProduceAsync("topico-teste", message);  // Produção assíncrona da mensagem no tópico especificado
+    var result = await producer.ProduceAsync("Cursos", message);  // Produção assíncrona da mensagem no tópico especificado
     Console.WriteLine($"{result.Offset}");  // Impressão do deslocamento (offset) da mensagem
     Console.WriteLine("Programa executado com sucesso!");  // Impressão de uma mensagem de sucesso
 }
