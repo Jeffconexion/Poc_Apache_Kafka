363ab490e3a29bbc799ce60510acfaf88915ad23
jeffconexion <jeff.debian2016@gmail.com>
2023-08-13 14:18:11 -0300

    Migração do rabbitmq para kafka.

---
+++ 4_CenarioDeExemplo/dev-store-main/src/building-blocks/DevStore.Core/Http/IRestClient.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace DevStore.Core.Http
+{
+    public interface IRestClient
+    {
+        Task<TResult> PostAsync<T, TResult>(T @event, string token = null);
+    }
+}
